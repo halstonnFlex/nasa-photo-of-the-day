14a5f6ae49a95d5343864f6011d7a5a917061569
Halstonn Landry <hallrstein@gmail.com>
2020-04-16 22:01:32 -0500

    mpv

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+import styled from 'styled-components';
 
 function App() {
+  const [data, setData]= useState ([]);
+ useEffect(()=>{
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+  .then(response=>{
+    setData(response.data);
+    console.log(response.data);
+  })
+  .catch(error=>{
+    console.log(error);
+  });
+ },[]) 
+const BodyDiv= styled.div`
+ height:100vh;
+ background-color:#000000;
+ background-image: linear-gradient(to top right, #ff00ff , #00ffff);
+ color:#ffff00;
+ text-shadow: 0px 0px 25px #ffff00;
+`;
+const ImgConfine= styled.img`
+width:60%;
+height:auto;
+`;
+const BoxBorder=styled.div`
+border: 5px solid #ffff00;
+box-shadow: inset 0px 0px 100px #ffff00
+`
+
   return (
-    <div className="App">
+    <BodyDiv className="App">
+
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        Date: {data.date}
       </p>
-    </div>
+      <p>
+        {data.explanation}
+      </p>
+      <BoxBorder>
+        <ImgConfine src={data.hdurl}/>
+      </BoxBorder>
+      <p>
+        {`copyright: ${data.copyright}`}
+      </p>
+    </BodyDiv>
   );
 }
 
